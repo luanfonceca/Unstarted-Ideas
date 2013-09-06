18173060b0a7ec39b794bea0a2fce11248a98ffb
Luan fonseca de farias <luanfonceca@gmail.com>
2013-09-06 20:14:21 -0300

    Adding the Idea Model and his migration and fixture.

--- ideas/models.py
+++ ideas/models.py
@@ -44,42 +44,49 @@ class Category(Model):
     #     return 'delete_category', (), {'category_id': self.pk}
 
 
-# class Idea(Model):
-#     name = CharField(max_length=100, verbose_name=_('Name'))
-#     slug = AutoSlugField(max_length=100, unique=True, blank=True, verbose_name=_('Slug'))
-#     created_at = CreationDateTimeField(verbose_name=_('Date Created'))
-#     modified_at = ModificationDateTimeField(verbose_name=_('Date Modified'))
-#     description = TextField(max_length=300, verbose_name=_('Description'))
-#     is_public = BooleanField(default=False, verbose_name=_('Is Public'))
-#     is_published = BooleanField(default=False, verbose_name=_('Pusblish Now'))
-#     views = PositiveIntegerField(verbose_name=_('Amount of Views'))
+class Idea(Model):
+    name = CharField(max_length=100, verbose_name=_('Name'))
+    slug = AutoSlugField(populate_from='name', verbose_name=_('Slug'))
+    created_at = CreationDateTimeField(verbose_name=_('Date Created'))
+    modified_at = ModificationDateTimeField(
+        verbose_name=_('Date Modified'))
+    description = TextField(max_length=300, 
+        verbose_name=_('Description'))
+    is_public = BooleanField(default=False, 
+        verbose_name=_('Is Public'))
+    is_published = BooleanField(default=False, 
+        verbose_name=_('Pusblish Now'))
+    views = PositiveIntegerField(default=0, 
+        verbose_name=_('Amount of Views'))
 
-#     # relations
-#     # license = ForeignKey(to=License, related_name='ideas', verbose_name=_('Licenses'))
-#     # patent = ForeignKey(to=Patent, related_name='ideas', verbose_name=_('Patents'))
-#     user = ForeignKey(to=User, related_name='ideas', verbose_name=_('Author'))
-#     categories = ManyToManyField(to=Category, related_name='ideas', verbose_name=_('Category'))
+    # relations
+    # license = ForeignKey(to=License, related_name='ideas', verbose_name=_('Licenses'))
+    # patent = ForeignKey(to=Patent, related_name='ideas', verbose_name=_('Patents'))
+    user = ForeignKey(to=User, related_name='ideas', 
+        verbose_name=_('Author'))
+    categories = ManyToManyField(to=Category, related_name='ideas', 
+        verbose_name=_('Category'))
 
-#     class Meta:
-#         db_table = 'idea'
-#         verbose_name = _('Idea')
-#         verbose_name_plural = _('Ideas')
+    class Meta:
+        db_table = 'idea'
+        verbose_name = _('Idea')
+        verbose_name_plural = _('Ideas')
 
-#     # def __unicode__(self):
-#     #     return u"%s" % self.name
+    def __unicode__(self):
+        return u"%s" % self.name
 
-#     # @permalink
-#     # def get_absolute_url(self):
-#     #     return 'idea_detail', (), {'idea_slug': self.slug}
+    # @permalink
+    # def get_absolute_url(self):
+    #     return 'idea_detail', (), {'idea_slug': self.slug}
 
-#     # @permalink
-#     # def get_json_url(self):
-#     #     return 'idea-detail', (), {'pk': self.pk}
+    # @permalink
+    # def get_json_url(self):
+    #     return 'idea-detail', (), {'pk': self.pk}
 
-#     # @permalink
-#     # def get_edit_url(self):
-#     #     return 'edit_idea', (), {'idea_id': self.pk}
+    # @permalink
+    # def get_edit_url(self):
+    #     return 'edit_idea', (), {'idea_id': self.pk}
 
-#     # @permalink
-#     # def get_delete_url(self):
-#     #     return 'delete_idea', (), {'idea_id': self.pk}
+    # @permalink
+    # def get_delete_url(self):
+    #     return 'delete_idea', (), {'idea_id': self.pk}
